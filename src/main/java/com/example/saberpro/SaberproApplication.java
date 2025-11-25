ac57a100011de91d560ab3f30e267ee77a68e964
aHeavenPlace <146133698+aHeavenPlace@users.noreply.github.com>
2025-11-25 01:26:04 -0500

    Refactor SaberproApplication main method formatting
    Removed unnecessary whitespace and added comment.

--- src/main/java/com/example/saberpro/SaberproApplication.java
+++ src/main/java/com/example/saberpro/SaberproApplication.java
@@ -3,11 +3,9 @@ package com.example.saberpro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   // sin scanBasePackages raro
 public class SaberproApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SaberproApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SaberproApplication.class, args);
+    }
 }
